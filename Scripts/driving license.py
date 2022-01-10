555441fbd49e1b125e370773bbd6f5843e61d214
Ai2033 <96627232+Ai2033@users.noreply.github.com>
2022-01-10 20:26:26 +0530

    Delete driving license.py

--- Scripts/driving license.py
+++
@@ -1,7 +0,0 @@
-name=input("Enter your name:")
-age=int(input("Enter your age:"))
-if age>=18:
-    print("You are eligible to apply for a driving license.")
-else:
-    print("You are not eligible to apply for a driving license.")
-            
